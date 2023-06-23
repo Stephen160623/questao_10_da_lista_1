504325797bf844546f087607e7677f98314c0767
Stephen160623 <136847798+Stephen160623@users.noreply.github.com>
2023-06-23 16:52:52 -0300

    Add files via upload

---
+++ questao1.10.naoterminada.py
@@ -0,0 +1,17 @@
+print("Programa para resolver ax^2 + bx + c = y")
+
+# Atribuição de variáveis
+
+# Entrada de dados
+
+cap = float(input("Valor do capital: "))
+t = float(input("Prazo do investimento em anos: "))
+r = float(input("Taxa de juros em percentagem: "))
+
+# Processamento de dados
+
+v = cap*(1+(r/100))**t
+
+# Saída de dados
+
+print("O valor capitalizado é R$)
